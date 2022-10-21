36385710709ea5bdeacaea5618daace42eee97e4
hug.sastrepro@gmail.com <hug.sastrepro@gmail.com>
2022-10-21 11:57:16 +0200

    V2

--- SASTRE_HUGO_PYTHON_18_10_22.py
+++ SASTRE_HUGO_PYTHON_18_10_22.py
@@ -242,5 +242,192 @@ def exo_12():
     print(Malist)
 #exo_12()
 
+#----------------------------------------------------------------------------------
+# COUR DU 21/10/2022
+
+# Ma_Liste = ["fraise","orange","banane"]
+# Ma_Liste.sort()
+# print(Ma_Liste)
+
+# Ma_Liste_num = [1, 30, 12, 14,18 ,47 ,4 ,6]
+# Ma_Liste_num.sort()
+# print(Ma_Liste_num)
+
+# Ma_Liste_num = [1, 30, 12, 14,18 ,47 ,4 ,6]
+# Ma_Liste_num.sort(reverse=True)
+# print(Ma_Liste_num)
+
+# def myfunc(x):
+#     return abs(x-50)
+# la_liste = [100, 50, 65, 82, 23]
+# la_liste.sort(key=myfunc)
+# print(la_liste)
+# attention a la case elle compte aussi pour le sort
+
+#faire une copie d'une liste
+# la_liste2 = [100, 50, 65, 82, 23]
+# la_liste2c = la_liste2.copy()
+# print(la_liste2c)
+
+# faire une jointure de liste
+# liste1 = ["x", "y", "z"]
+# liste2 = [1,2,3]
+# liste3 = liste1 + liste2
+# print(liste3)
+
+# autre methode
+# liste1 = ["x", "y", "z"]
+# liste2 = [1,2,3]
+
+# for x in liste2:
+#     liste1.append(x)
+# print(liste1)
+
+#autre methode bis
+# liste1.extend(liste2)
+# print(liste1)
+
+#clear() va vider la liste
+# liste1 = ["x", "y", "z"]
+# liste1.clear()
+# print(liste1)
+
+# LES TUPLES
+# montuple = ('Pomme','Kiwi','Citron')
+# print(montuple)
+# print(type(montuple))
+# print(len(montuple))
+
+# crée un tuple avec un objet
+# mon_tuple = ("poire",) # si on enleve la virgule on recoit un string et non plus un string
+# print(type(mon_tuple))
+
+# On peut mélanger les types et avoir des tuples constitués de booléen, chaine, entier ....
+# Le tuple est une classe comme les autres types en python
+#faire appel au constructeur du tuple
+# montuple =  tuple(("Pomme", "Banane", "Fraise"))
+# print(montuple)
+# print(montuple[1])
+# print(montuple[-1]) # retourne le dernier élément de mon tuple
+
+# My_tuple = ("Pomme","Kiwi","Citron","Banane","Fraise","Melon","Cerise")
+# print(My_tuple[2:5]) #On accede aux élément de 2 a 4 !!
+
+# My_tuple = ("Pomme","Kiwi","Citron","Banane","Fraise","Melon","Cerise")
+# print(My_tuple[2:]) #On accede aux élément de Citron a la fin de notre tuple
+
+# My_tuple = ("Pomme","Kiwi","Citron","Banane","Fraise","Melon","Cerise")
+# if "Citron" in My_tuple: # RECHERCHE DE CITRON DANS MA LISTE
+#     print("Présent !!! ")
+
+# montuple = ("Pomme","Kiwi","Citron")
+# a = list(montuple)
+# a.append("Ballon") # ajout de ballon a la fin de mon tuple
+# a[1]="fraise"
+# montuple=tuple(a)
+# print(montuple) # modification de mon tuple
+
+#Ajouter un tuple à un tuple
+# montuple = ("Pomme", "Kiwi", "Citron")
+# a =("Fraise",)
+# montuple+=a
+# print(montuple)
+#
+# #Supprimer un élément
+# montuple = ("Pomme", "Kiwi", "Citron")
+# a =list(montuple)
+# a.remove("Pomme")
+# montuple = tuple(a)
+# print(montuple)
+
+# montuple = ("Pomme", "Kiwi", "Citron")
+# del montuple
+# print(montuple) # erreur car mon tuple n'existe plus
+
+#montuple = ("Pomme", "Kiwi", "Citron")
+# (a,b,*c) = montuple # récupérer des variables d'un tuple mais pour le c on récupere une list
+# print(a)
+# print(b)
+# print(c) # recupere une liste
+
+# Parcourir un tuple via une boucle
+# for x in montuple:
+#     print(x)
+
+
+# Parcrourir un tuple utilisant le numéro d'index
+# for x in range(len(montuple)):
+#     print(montuple[x])
+
+# parcourir avec un while
+# x=0
+# while x < len(montuple):
+#     print(montuple[x])
+#     x = x+1
+
+# montuple1 = ("Pomme", "Kiwi","Fraise")
+# montuple2 = (4, 12, 6)
+# montuple3 = montuple1 + montuple2
+# print(montuple3)
+# montuple4 = montuple1 * 2
+# print(montuple4)
+
+def Affiche_Nombre_Pairs():
+    Ma_liste = []
+    x=1
+    while x < 101 :
+        if x % 2 ==0:
+            Ma_liste.append(x)
+            x = x+1
+        else :
+            x=x+1
+    print(Ma_liste)
+#Affiche_Nombre_Pairs()
+
+def Lancer_Des_Aleatoires():
+    Lancer_Des = 0
+    Liste_Des = []
+    while Lancer_Des <=6 :
+        Mon_Des = random.randrange(1,7)
+        Liste_Des.append(Mon_Des)
+        Lancer_Des = Lancer_Des +1
+    print(Liste_Des)
+#Lancer_Des_Aleatoires()
+
+def Lancer_Des_Aleatoires_V2():
+    Lancer_Des = int(input('Veuillez entrer votre nombre de lancé :'))
+    Liste_Des = []
+    x=0
+    Somme_Des =0
+    while x <Lancer_Des :
+        Mon_Des = random.randrange(1,7)
+        Liste_Des.append(Mon_Des)
+        Somme_Des = Somme_Des + Liste_Des[x]
+        x = x+1
+    Moy_Des = Somme_Des / Lancer_Des
+    print('Voici la liste de vos lancés : ', Liste_Des)
+    print('Votre moyenne de lancé est de : ', Moy_Des)
+#Lancer_Des_Aleatoires_V2()
+
+def Compteur_Lettre():
+    Mon_String = str(input("Entrez une phrase "))
+    Lettre = str(input('Veuillez entrez une lettre afin de la compter : '))
+    Compteur = 0
+    for x in range(len(Mon_String)):
+        if Lettre in Mon_String[x].lower():
+            Compteur = Compteur +1
+    print('Il y a :', Compteur, Lettre,'dans votre phrase')
+#Compteur_Lettre()
+
+def Compteur_lettre_V2():
+    Mon_Alpahbet =[['a'],['b'],'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    Mon_String = str(input("Entrez une phrase "))
+    Compteur = 0
+    for x in range(len(Mon_String)):
+        if Mon_Alpahbet[x] in Mon_String:
+            Compteur = Compteur +1
+    print('Il y a :', Compteur,'dans votre phrase')
+Compteur_lettre_V2()
+
 
 
